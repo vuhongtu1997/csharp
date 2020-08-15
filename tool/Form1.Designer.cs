b4742b066b67b2706085e1d614ee017d0ceca706
vuhongtu1997 <tuvuvu2121@gmail.com>
2020-08-15 07:56:06 +0700

    A Hưng đẹp trai

---
+++ tool/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.cb_Com = new System.Windows.Forms.ComboBox();
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.lb_State_Com = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.data_In);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // cb_Com
+            // 
+            this.cb_Com.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_Com.FormattingEnabled = true;
+            this.cb_Com.Location = new System.Drawing.Point(6, 35);
+            this.cb_Com.Name = "cb_Com";
+            this.cb_Com.Size = new System.Drawing.Size(142, 21);
+            this.cb_Com.TabIndex = 0;
+            // 
+            // btn_Connect
+            // 
+            this.btn_Connect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn_Connect.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Connect.Location = new System.Drawing.Point(258, 19);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(134, 94);
+            this.btn_Connect.TabIndex = 1;
+            this.btn_Connect.Text = "CONNECT";
+            this.btn_Connect.UseVisualStyleBackColor = false;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
+            // 
+            // lb_State_Com
+            // 
+            this.lb_State_Com.AutoSize = true;
+            this.lb_State_Com.BackColor = System.Drawing.Color.LightSalmon;
+            this.lb_State_Com.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_State_Com.Location = new System.Drawing.Point(6, 76);
+            this.lb_State_Com.Name = "lb_State_Com";
+            this.lb_State_Com.Size = new System.Drawing.Size(91, 16);
+            this.lb_State_Com.TabIndex = 2;
+            this.lb_State_Com.Text = "Disconnected";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_Connect);
+            this.groupBox1.Controls.Add(this.lb_State_Com);
+            this.groupBox1.Controls.Add(this.cb_Com);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Location = new System.Drawing.Point(12, 122);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(427, 134);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CONNECT";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(106, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 36);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Tool Test Đèn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1203, 567);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RD TEST";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox cb_Com;
+        private System.Windows.Forms.Button btn_Connect;
+        private System.Windows.Forms.Label lb_State_Com;
+        private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ tool/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.Linq.Expressions;
+
+namespace tool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string hexvalue = "";
+        int time;
+        int numbytes;
+        byte[] rxbyte;
+        int port_Name_length;
+        byte mac_High = 0;
+        byte mac_Low = 0;
+        byte mac_High_Old = 0;
+        byte mac_Low_Old = 0;
+        byte mac_High_RSP = 1;
+        byte mac_Low_RSP = 0;
+        int time_Check = 0;
+        int check_RSP = 0;
+        int time_Old = 0;
+        byte num_RSP = 0;
+        byte num_RSP_Old = 0;
+        byte[] check_node = { 0xe8, 0xff, 0x00, 0x00, 0x00, 0x00, 0x02, 0x01, 0xff, 0xff, 0x82, 0x4b };
+        byte[] dim_cct = { 0xe8, 0xff, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0xff, 0xff, 0x82, 0x5e, 0xff, 0xff, 0x20, 0x03, 0x00, 0x00, 0x00 };
+        byte[] off = { 0xe8, 0xff, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0xff, 0xff, 0x82, 0x02, 0x00, 0x00 };
+        int pass_Count = 0;
+        bool MACExit = false;
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time++;
+            string cb_Text = cb_Com.Text;
+            string[] port_Name = SerialPort.GetPortNames();
+
+            if (port_Name_length == 0)
+            {
+                btn_Connect.Enabled = false;
+                lb_State_Com.Text = "Disconnected";
+                lb_State_Com.BackColor = Color.LightSalmon;
+                btn_Connect.Text = "CONNECT";
+                serialPort1.Close();
+            }
+
+            if (port_Name_length != port_Name.Length)
+            {
+                port_Name_length = port_Name.Length;
+                {
+                    if (port_Name_length != 0)
+                    {
+                        cb_Com.Items.Clear();
+                        for (int i = 0; i < port_Name_length; i++)
+                        {
+                            cb_Com.Items.Add(port_Name[i]);
+                        }
+                        cb_Com.SelectedIndex = 0;
+
+                        bool compare_Text = false;
+
+                        for (int i = 0; i < port_Name.Length; i++)
+                        {
+                            if (string.Compare(cb_Text, port_Name[i], true) == 0)
+                            {
+                                compare_Text = true;
+                                serialPort1.Close();
+                            }
+                        }
+
+                        if (!compare_Text)
+                        {
+                            btn_Connect.Enabled = true;
+                            lb_State_Com.Text = "Disconnected";
+                            lb_State_Com.BackColor = Color.LightSalmon;
+                            btn_Connect.Text = "CONNECT";
+                            cb_Com.Enabled = true;
+                        }
+                    }
+                    else
+                    {
+                        btn_Connect.Enabled = false;
+                        lb_State_Com.Text = "Disconnected";
+                        lb_State_Com.BackColor = Color.LightSalmon;
+                        btn_Connect.Text = "CONNECT";
+                        cb_Com.Text = null;
+                        cb_Com.Items.Clear();
+                        serialPort1.Close();
+                    }
+                }
+            }
+
+            if (!serialPort1.IsOpen)
+            {
+                MACExit = false;
+            }
+
+            if (MACExit == false && serialPort1.IsOpen)
+            {
+                mac_High_RSP = 0;
+                mac_Low_RSP = 0;
+                check_RSP = 0;
+                serialPort1.Write(check_node, 0, 12);
+            }
+
+            if (MACExit && serialPort1.IsOpen)
+            {                
+                mac_Low_Old = mac_Low;
+                mac_High_Old = mac_High;
+                if (time - time_Check == 10 && check_RSP == 0)
+                {
+                    dim_cct[12] = dim_cct[13] = 0xff;
+                    dim_cct[8] = mac_Low_Old;
+                    dim_cct[9] = mac_High_Old;
+                    dim_cct[15] = 0x03;
+                    serialPort1.Write(dim_cct, 0, 19);
+                    mac_High_RSP = 0;
+                    mac_Low_RSP = 0;
+                    time_Old = time;
+                }
+
+                if (time - time_Check == 25 && check_RSP == 1)
+                {
+                    dim_cct[12] = 0x66;
+                    dim_cct[13] = 0x26;
+                    serialPort1.Write(dim_cct, 0, 19);
+                    mac_High_RSP = mac_Low_RSP = 0;
+                    time_Old = time;
+                }
+
+                if (time - time_Check == 40 && check_RSP == 2)
+                {
+                    dim_cct[12] = 0x66;
+                    dim_cct[13] = 0x26;
+                    dim_cct[15] = 0x4e;
+                    serialPort1.Write(dim_cct, 0, 19);
+                    mac_High_RSP = mac_Low_RSP = 0;
+                    time_Old = time;
+                }
+
+                if (time - time_Check == 55 && check_RSP == 3)
+                {
+                    dim_cct[12] = dim_cct[13] = 0xff;
+                    dim_cct[15] = 0x4e;
+                    serialPort1.Write(dim_cct, 0, 19);
+                    mac_High_RSP = mac_Low_RSP = 0;
+                    time_Old = time;
+                }
+                
+
+                if (check_RSP == 4 && time - time_Check == 65)
+                {
+                    off[8] = mac_Low_Old;
+                    off[9] = mac_High_Old;
+                    serialPort1.Write(off, 0, 14);
+                    time_Check = time;
+                    MACExit = false;
+                    mac_High_RSP = 0;
+                    mac_Low_RSP = 0;
+                    time_Old = time;
+                    pass_Count++;
+                }
+
+                if (time - time_Old >= 40)
+                {
+                    time_Check = time;
+                    MACExit = false;
+                    mac_High_RSP = 0;
+                    mac_Low_RSP = 0;
+                    time_Old = time;
+                    num_RSP_Old = num_RSP = 0;
+                }
+            }
+
+            if (check_RSP == 5)
+            {
+                time_Check = time;
+                MACExit = false;
+                mac_High_RSP = 0;
+                mac_Low_RSP = 0;
+                time_Old = time;
+                num_RSP_Old = num_RSP = 0;
+            }
+
+            if (mac_Low_RSP == mac_Low_Old && mac_High == mac_High_Old && num_RSP != num_RSP_Old)
+            {
+                check_RSP++;
+                num_RSP_Old = num_RSP;
+                mac_Low_RSP = mac_High_RSP = 0;
+            }
+
+            /*if (check_RSP == 3)
+            {
+                pass_Count++;
+                lb_Pass.Text = "Pass: " + Convert.ToString(pass_Count);
+            }*/
+        }
+
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            btn_Connect.Enabled = true;
+            if (lb_State_Com.Text == "Disconnected")
+            {
+                serialPort1.PortName = cb_Com.Text;
+                serialPort1.Open();
+                btn_Connect.Text = "DISCONNECT";
+                lb_State_Com.Text = "Connected";
+                lb_State_Com.BackColor = Color.LightGreen;
+                cb_Com.Enabled = false;
+            }
+            else
+            {
+                serialPort1.Close();
+                btn_Connect.Text = "CONNECT";
+                lb_State_Com.Text = "Disconnected";
+                lb_State_Com.BackColor = Color.LightSalmon;
+                cb_Com.Enabled = true;
+            }
+        }
+
+        private void data_In(object sender, SerialDataReceivedEventArgs e)
+        {
+            Thread.Sleep(30);
+            numbytes = serialPort1.BytesToRead;
+            rxbyte = new byte[numbytes];
+            if (numbytes >= 8)
+            {
+                for (int i = 0; i < numbytes; i++)
+                {
+                    rxbyte[i] = (byte)serialPort1.ReadByte();
+                }
+                if (rxbyte[0] == 0x0a && rxbyte[3] == 0x81)
+                {
+                    mac_Low = rxbyte[4];
+                    mac_High = rxbyte[5];
+                    time_Check = time;
+                    MACExit = true;
+                }
+                if (rxbyte[0] == 0x0f && rxbyte[3] == 0xb5)
+                {
+                    mac_Low_RSP = rxbyte[6];
+                    mac_High_RSP = rxbyte[7];
+                    num_RSP = rxbyte[16];     
+                }
+                foreach (byte b in rxbyte)
+                {
+                    hexvalue = hexvalue + (b.ToString("X2")) + "";
+                }
+                this.Invoke(new EventHandler(Showdata));
+                hexvalue = "";
+            }
+
+        }
+
+        private void Showdata(object sender, EventArgs e)
+        {
+            
+           
+        }
+    }
+}
